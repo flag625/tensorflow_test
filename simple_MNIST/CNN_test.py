e30bbd9c277ff822ce98abc2b1cee21e551e5cdb
flag625 <liangzhisen@outlook.com>
2018-08-23 17:45:02 +0800

    Tensorflow框架练习

---
+++ simple_MNIST/CNN_test.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+from simple_MNIST import input_data
+
+"""
+    简单的MNIST手写数字识别模型CNN。
+    来源：http://www.tensorfly.cn/tfdoc/tutorials/mnist_pros.html
+"""
+
+class simple_CNN(object):
+    def __init__(self):
+        self.x = tf.placeholder("float", shape=[None, 784])
+        self.y_ = tf.placeholder("float", [None, 10])
+        self.mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+
+    def weight_variable(self, shape):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial)
+
+    def bias_varible(self, shape):
+        initial = tf.constant(0.1, shape=shape)
+        return tf.Variable(initial)
+
+    def conv2d(self, x, W):
+        return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
+
+    def max_pool_2x2(self, x):
+        return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1],
+                              padding='SAME')
+
+    def conv1(self):
+        W_conv1 = self.weight_variable([5,5,1,32])
+        b_conv1 = self.bias_varible([32])
+        x_image = tf.reshape(self.x, [-1,28,28,1])
+        h_conv1 = tf.nn.relu(self.conv2d(x_image, W_conv1) + b_conv1)
+        h_pool1 = self.max_pool_2x2(h_conv1)
+        return h_pool1
+
+    def conv2(self):
+        W_conv2 = self.weight_variable([5,5,32,64])
+        b_conv2 = self.bias_varible([64])
+
+        h_conv2 = tf.nn.relu(self.conv2d(self.conv1(), W_conv2) + b_conv2)
+        h_pool2 = self.max_pool_2x2(h_conv2)
+        return h_pool2
+
+    def dense_con(self):
+        W_fc1 = self.weight_variable([7*7*64, 1024])
+        b_fc1 = self.bias_varible([1024])
+
+        h_pool2_flat = tf.reshape(self.conv2(), [-1,7*7*64])
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1)+b_fc1)
+        return h_fc1
+
+    def dropout(self):
+        self.keep_prob = tf.placeholder("float")
+        h_fc1_drop = tf.nn.dropout(self.dense_con(), self.keep_prob)
+        return h_fc1_drop
+
+    def output(self):
+        W_fc2 = self.weight_variable([1024, 10])
+        b_fc2 = self.bias_varible([10])
+
+        y_conv = tf.nn.softmax(tf.matmul(self.dropout(), W_fc2)+b_fc2)
+        return y_conv
+
+    def loss(self):
+        cross_entropy = -tf.reduce_sum(self.y_ * tf.log(self.output()))
+        return cross_entropy
+
+    def accuracy(self):
+        correct_prediction = tf.equal(tf.argmax(self.output(), 1), tf.argmax(self.y_, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        return accuracy
+
+    def train(self):
+        train_step = tf.train.AdadeltaOptimizer(1e-4).minimize(self.loss())
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for i in range(2000):
+                batch = self.mnist.train.next_batch(50)
+                if i%100 == 0:
+                    train_accuracy = self.accuracy().eval(feed_dict={
+                        self.x: batch[0], self.y_: batch[1], self.keep_prob: 1.0
+                    })
+                    print("step %d, training accuracy %g" %(i, train_accuracy))
+                train_step.run(feed_dict={self.x: batch[0], self.y_: batch[1],
+                                          self.keep_prob: 0.5})
+
+    def test(self):
+        self.accuracy().eval(feed_dict={self.x: self.mnist.test.images,
+                                        self.y_: self.mnist.test.labels,
+                                        self.keep_prob: 1.0})
+
+
+
+if __name__ == "__main__":
+    cnn = simple_CNN()
+    cnn.train()
+    cnn.test()
+
+
+
---
+++ simple_MNIST/MNIST_test.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+from simple_MNIST import input_data
+
+"""
+    简单的MNIST手写数字识别模型。
+    来源：http://www.tensorfly.cn/tfdoc/tutorials/mnist_beginners.html
+"""
+
+class simple_MNIST(object):
+    def __init__(self):
+        self.x = tf.placeholder("float", [None, 784])
+        self.W = tf.Variable(tf.zeros([784, 10]))
+        self.b = tf.Variable(tf.zeros([10]))
+        self.y = tf.nn.softmax(tf.matmul(self.x, self.W)+self.b)
+        self.y_ = tf.placeholder("float", [None, 10])
+        self.cross_entropy = -tf.reduce_sum(self.y_*tf.log(self.y))
+        self.train_step = tf.train.GradientDescentOptimizer(0.01).minimize(self.cross_entropy)
+        self.mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+    def train(self):
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for i in range(10000):
+                batch_xs, batch_ys = self.mnist.train.next_batch(100)
+                sess.run(self.train_step, feed_dict={self.x: batch_xs, self.y_: batch_ys})
+
+    def test(self):
+        correct_prediction = tf.equal(tf.argmax(self.y, 1), tf.argmax(self.y_, 1))
+        accuarcy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            print(sess.run(accuarcy, feed_dict={self.x: self.mnist.test.images, self.y_: self.mnist.test.labels}))
+
+if __name__ == "__main__":
+    mnist = simple_MNIST()
+    mnist.train()
+    mnist.test()
+
+
+
+
